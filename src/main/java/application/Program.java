3fc516a0df3ad36d0086e9e2ba439af0a8c2643b
Jackson R Wippel <jrwippel@hotmail.com>
2021-12-30 14:21:07 -0300

    Acesso ao banco de dados e listagem de dados do banco

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,34 @@
+package application;
+
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+
+		try {
+			connection = DB.getConnection();
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery("select * from department");
+			while (resultSet.next()) {
+				System.out.println(resultSet.getInt("Id")+","+ resultSet.getString("Name"));
+			}
+
+		}catch (SQLException e){
+			e.printStackTrace();
+		}finally {
+			DB.closeConnection();
+			DB.closeResultSet(resultSet);
+			DB.closeStatement(statement);
+		}
+	}
+}
